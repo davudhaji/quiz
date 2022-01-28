3520e060803a75717304abbdb1429a714939a80c
ClassTime <classtime@code.local>
2022-01-28 16:47:22 +0400

    first commit

---
+++ WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class AppDbContext:IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+        }
+
+        public DbSet<CustomUser> CustomUsers { get; set; }
+        public DbSet<Work> Works { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Work.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Work
+    {
+        [Key]
+        public int id { get; set; }
+        [MaxLength(50)]
+        public string Title { get; set; }
+        [MaxLength(500)]
+        public string Content { get; set; }
+
+        [MaxLength(50)]
+        public string Image { get; set; }
+
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+    }
+}
